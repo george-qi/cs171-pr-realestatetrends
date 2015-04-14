244aaa6d9c35472a6e1950980ad1eeba3415d905
Jacob Kim <jacobkim@Jacobs-MacBook-Pro.local>
2015-04-14 17:10:51 -0400

    finished json

--- data/data_wrangle.py
+++ data/data_wrangle.py
@@ -1,65 +1,97 @@
 import csv
+import json
 
-csv_filename = 'allhomes.csv'
+def init_data(fname):
+	csv_filename = fname +'.csv'
 
-with open(csv_filename, 'r') as csv_fh:
-	# Parse as a CSV file.
-	reader = csv.reader(csv_fh)
+	with open(csv_filename, 'rU') as csv_fh:
+		# Parse as a CSV file.
+		reader = csv.reader(csv_fh)
 
-	# Skip the header line.
+		# Skip the header line.
+		header = next(reader, None)
+
+		# Loop over the file.
+		data = []
+
+		for row in reader:
+			months_index = range(1,len(header))
+			months = []
+
+			for i in months_index:
+				if len(row[i]) > 0:
+					zhvi = row[i]
+				else:
+					zhvi = -1
+
+				month = {
+					'month': header[i],
+					'allhomes': int(zhvi),
+					'1br': -1,
+					'2br': -1,
+					'3br': -1,
+					'4br': -1
+				}
+
+				months.append(month)
+
+			item = {
+				'city': row[0],
+				'months': months
+			}
+
+			data.append(item)
+
+	return data 
+
+def add_data(fname,df):
+	next_csv = fname +'.csv'
+
+	fobject = open(next_csv, 'rU')
+	reader = csv.reader(fobject)
 	header = next(reader, None)
 
 	# Loop over the file.
-	data = []
-
 	for row in reader:
-		months_index = range(4,len(header))
-		months = []
+		months_index = range(1,len(header))
 
-		for i in months_index:
-			if len(row[i]) > 0:
-				zhvi = row[i]
-			else:
-				zhvi = -1
+		city_index = [i for i, j in enumerate(df) if j['city'] == row[0]]
+		
+		if len(city_index) > 0:
+			city_index = city_index[0]
 
-			month = {
-				'month': header[i],
-				'allhomes': int(zhvi)
-			}
+			for i in months_index:
+				m_index = [k for k, j in enumerate(df[city_index]['months']) if j['month'] == header[i]]
+				m_index = m_index[0]
 
-			months.append(month)
+				if len(row[i]) > 0:
+					br = row[i]
+				else:
+					br = -1
 
-		item = {
-			'city': row[0],
-			'state': row[1],
-			'months': months
-		}
+				df[city_index]['months'][m_index][fname] = int(br)
 
-		data.append(item)
+	return df
 
-next_csv = '1br.csv'
+# read data from CSV
+data = init_data('allhomes')
+files = ['1br','2br','3br','4br','5br']
 
-fobject = open(next_csv, 'r')
-reader = csv.reader(fobject)
-header = next(reader, None)
+for f in files:
+	data = add_data(f,data)
 
-# Loop over the file.
-for row in reader:
-	months_index = range(4,len(header))
+# save json file
+with open('data.json', 'w') as outfile:
+    json.dump(data, outfile)
 
-	city_index = [i for i, j in enumerate(data) if j['city'] == row[0]]
-	
-	if len(city_index) > 0:
-		city_index = city_index[0]
+# open json file
+# from pandas.io.json import json_normalize
 
-		for i in months_index:
-			m_index = [k for k, j in enumerate(data[city_index]['months']) if j['month'] == header[i]]
-			m_index = m_index[0]
+# with open('data.json') as data_file:    
+#     d2 = pd.read_json(data_file)
 
-			if len(row[i]) > 0:
-				br = row[i]
-			else:
-				br = -1
 
-			data[city_index]['months'][m_index]['1br'] = int(br)
+# json_normalize(d2['months'])
+
+
 
